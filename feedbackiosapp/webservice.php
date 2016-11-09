f40e5b4d76614c07d352132c25b8329e8f41e4c6
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-09 17:50:54 +0530

    Update webservice.php

--- feedbackiosapp/webservice.php
+++ feedbackiosapp/webservice.php
@@ -2,13 +2,13 @@
 
 define("vikas.y@moldtekindia.com.usgt2", "Your salesforce username");
 define("mttl@123", "salesforce password");
-define("SECURITY_TOKEN", "salesforce security token");
+
 
 require_once ('soapclient/SforcePartnerClient.php');
 
 $mySforceConnection = new SforcePartnerClient();
 $mySforceConnection->createConnection("PartnerWSDL.xml");
-$mySforceConnection->login(vikas.y@moldtekindia.com.usgt2, mttl@123.SECURITY_TOKEN);
+$mySforceConnection->login(vikas.y@moldtekindia.com.usgt2, mttl@123);
 
 $query = "SELECT Id, Name, Product_Description__c from Product__c";
 $response = $mySforceConnection->query($query);
