b4f2b8d51a71c2e62be8a750888d7dadd40eae77
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-09 19:08:55 +0530

    Update webservice.php

--- feedbackiosapp/webservice.php
+++ feedbackiosapp/webservice.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('182.50.151.4', 'gadvise', 'p@ssw0rd');
+$link = mysqli_connect('ec2-54-243-47-46.compute-1.amazonaws.com', 'awskdtjguszwxc', 'UyHMcmVteZBhy79KkdcZKG-Fae');
 if (!$link) {
     die('Could not connect: ' . mysqli_error());
 }
