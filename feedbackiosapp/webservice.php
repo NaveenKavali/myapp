7e5b4d889c2d22e1a05699c1a17954c03043acde
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-09 18:21:05 +0530

    Update webservice.php

--- feedbackiosapp/webservice.php
+++ feedbackiosapp/webservice.php
@@ -1,7 +1,7 @@
 <?php
-$link = mysqli_connect('localhost', 'gadvise', 'p@ssw0rd');
+$link = mysqli_connect('166.62.28.136', 'gadvise', 'p@ssw0rd');
 if (!$link) {
-    die('Could not connect: ' . mysql_error());
+    die('Could not connect: ' . mysqli_error());
 }
 echo 'Connected successfully';
 mysqli_close($link);
