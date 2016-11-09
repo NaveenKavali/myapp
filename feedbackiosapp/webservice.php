7dc01fa5bae2c8045d90f24587298a52767ab04a
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-09 18:09:49 +0530

    Update webservice.php

--- feedbackiosapp/webservice.php
+++ feedbackiosapp/webservice.php
@@ -1,21 +1,4 @@
 <?php
-
-require_once ('soapclient/SforcePartnerClient.php');
-
-$mySforceConnection = new SforcePartnerClient();
-$mySforceConnection->createConnection("PartnerWSDL.xml");
-$mySforceConnection->login(vikas.y@moldtekindia.com.usgt2, mttl@123);
-
-$query = "SELECT Id, Name, Product_Description__c from Product__c";
-$response = $mySforceConnection->query($query);
-
-foreach ($response->records as $record)
-{
-echo '<tr>
-	<td>'.$record->Id.'</td>
-	<td>'.$record->fields->Name.'</td>
-	<td>'.$record->fields->Product_Description__c.'</td>
-	 </tr>';
- }
-
+mysql_connect("localhost","root","123");
+mysql_select_db("hello");
 ?>
