ebc3ebad4f74d6f73f387056fec254e57fd9ee15
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-14 12:39:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
-echo 'Hi Master';
+define ("salesforce_username", 'vikas.y@moldtekindia.com.usgt2');
+define ("salesforce_password", 'mttl@123');
+define ("salesforce_wsdl", 'soapclient/partner.wsdl.xml');
+ 
+require_once ('soapclient/SforcePartnerClient.php');
 
 ?>
