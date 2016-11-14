d71be85a8877c25597032ed6fb35e745505a8e96
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-14 18:28:01 +0530

    Update webservice.php

--- feedbackiosapp/webservice.php
+++ feedbackiosapp/webservice.php
@@ -1,6 +1,6 @@
 <?php 
 
-$dbconn3 = pg_connect("host=ec2-54-243-47-46-compute-1.amazonaws.com port=5432 dbname=d1pdd9cb9tq855 user=vikas.y@moldtekindia.com.usgt2 password=mttl@123");
+$dbconn3 = pg_connect("host=ec2-54-243-47-46-compute-1.amazonaws.com dbname=d1pdd9cb9tq855 user=vikas.y@moldtekindia.com.usgt2 password=mttl@123");
 if($dbconn3){
 echo 'connected';
 }
