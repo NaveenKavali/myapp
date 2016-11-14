7f97579c7b7e2d05e6e860678d57263cd297067b
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-14 16:13:51 +0530

    Update webservice.php

--- feedbackiosapp/webservice.php
+++ feedbackiosapp/webservice.php
@@ -5,8 +5,6 @@
 // headers to tell that result is JSON
 header('Content-type: application/json');
 
-// send the result now
-echo json_encode($result_json);
  $json = file_get_contents('https://feedbackiosapp.herokuapp.com'); 
  $obj = json_decode($json, true);
 print_r($obj);
