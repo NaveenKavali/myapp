12a73324c6bb8004d1cf22839d681822e0f29b6d
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-14 16:19:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-$result_json = array('name' => 'naveen', 'age' => '16');
-
-
 // headers to tell that result is JSON
 header('Content-type: application/json');
 
-// send the result now
-echo json_encode($result_json);
+ $json = file_get_contents('https://feedbackiosapp.herokuapp.com/feedbackiosapp/webservice.php'); 
+ $obj = json_decode($json, true);
+print_r($obj);
 ?>
