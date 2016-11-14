80a708aedaac91403bf9507afd755d7fa55d9f76
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-14 13:47:06 +0530

    Update webservice.php

--- feedbackiosapp/webservice.php
+++ feedbackiosapp/webservice.php
@@ -1,10 +1,1 @@
-
-<?php
-    $db_connection = pg_connect("host=ec2-54-243-47-46.compute-1.amazonaws.com dbname=d1pdd9cb9tq855 user=awskdtjguszwxc password=UyHMcmVteZBhy79KkdcZKG-Fae");
-
-if (!$db_connection) {
-    die('Could not connect: ' . mysqli_error());
-}
-echo 'Connected successfully';
-
-?>
+<?php phpinfo(); ?>
