c7cbbb94f4cb868986398aa290800c5d932c3065
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-14 16:30:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
+
+$url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$parts = parse_url($url);
+parse_str($parts['query'], $query);
+echo $query['hi'];
 // headers to tell that result is JSON
 header('Content-type: application/json');
 
- $json = file_get_contents('https://feedbackiosapp.herokuapp.com/feedbackiosapp/webservice.php'); 
  $obj = json_decode($json, true);
 print_r($obj);
 ?>
